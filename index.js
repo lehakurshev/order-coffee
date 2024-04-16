49bdb15798012bffc115bd0bc0cd88f5cc6decf0
Ivan Fedorov <fedivmi@gmail.com>
2024-04-16 17:29:03 +0500

    task 3,4,5

--- index.js
+++ index.js
@@ -21,3 +21,36 @@ addBtn.addEventListener("click", () => {
 
     forms[forms.length - 1].after(newForm);
 });
+
+document.querySelector('.submit-button').addEventListener('click', (event) => {
+    event.preventDefault();
+    openModal();
+});
+
+document.querySelector('.close-model').addEventListener('click', () => {
+    closeModal();
+});
+
+function openModal() {
+    const overlay = document.querySelector('.overlay');
+    overlay.style.visibility = 'visible';
+    document.querySelector('.status-order').textContent = getDrinksAmount();
+}
+
+
+function closeModal() {
+    const overlay = document.querySelector('.overlay');
+    overlay.style.visibility = 'hidden';
+}
+
+function getDrinksAmount() {
+    const num = document.querySelectorAll('fieldset').length;
+    const mod = num % 10;
+    const drink = `${num !== 11 && mod === 1 ?
+        'напиток'
+        : (num > 20 && (mod === 2 || mod === 3 || mod === 4)) || (num >= 2 && num <= 4) ?
+            'напитка'
+            : 'напитков'}`
+
+    return `Заказ принят! Вы заказали ${num} ${drink}`;
+}
